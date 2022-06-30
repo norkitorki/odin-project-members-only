96f86e42fa9279f9e490644e7669688447127463
norkitorki <norki@tuta.io>
2022-06-30 07:51:33 +0200

    Configure devise to make username the primary authentication key

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_29_090323) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
