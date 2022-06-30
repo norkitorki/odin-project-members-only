6e900a104314d7db3e670990520e83f94d2ea84b
norkitorki <norki@tuta.io>
2022-06-30 07:46:50 +0200

    Add username uniqueness model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username,
     presence: true,
+    uniqueness: true,
     length: { maximum: 40 }
+
   has_many :posts
 end
