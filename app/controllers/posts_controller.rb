789dc91f7616f1c1f8f37cb8ff85a94cee99fe44
norkitorki <norki@tuta.io>
2022-07-08 07:40:26 +0200

    Use except instead of only for set_post controller filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update update_rating destroy ]
+  before_action :set_post, except: %i[ index create ]
   before_action :authenticate_user!, except: %i[ index show update_rating ]
 
   def index
